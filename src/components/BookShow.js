95460d2ca68126045fde26c64a1c3d469589c6ca
blur <thetobiterra@gmail.com>
2023-01-30 00:19:32 +0100

    build

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,16 @@
-function BookShow({ book }) {
+function BookShow({ book, onDelete }) {
+    
+    const handleClick = () =>{
+        onDelete(book.id)
+    }
     return (
         <div className='book-show'>
             {book.title}
+            <div className='actions'>
+                <button className="delete" onClick={handleClick}>
+                    Delete
+                </button>
+            </div>
         </div>
     )
 }
