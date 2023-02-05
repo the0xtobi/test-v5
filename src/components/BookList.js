0c620c4d5fdbbe24ef2ca1627bbd620d8a58ce71
blur <thetobiterra@gmail.com>
2023-02-05 23:35:58 +0100

    build

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,12 @@
 import BookShow from './BookShow'
 
 function BookList({ books, onDelete }) {
-    const renderedBooks = books.map((book) => {
-        return <BookShow onDelete={onDelete} key={book.id} book={book} />
-    })
+
     return (
         <div className='book-list'>
-            {renderedBooks}
+            {books.map((book) => {
+                return <BookShow onDelete={onDelete} key={book.id} book={book} />
+            })}
         </div>
     )
 }
