219e231c22187bb73582c0ffb45149bbc42f1efa
blur <thetobiterra@gmail.com>
2023-02-06 11:25:34 +0100

    build

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,7 +1,24 @@
-function BookEdit(){
-    return(
-        <form className='book-edit'>
+import { useState } from 'react'
 
+function BookEdit() {
+    const [title, setTitle] = useState('')
+
+    const handleChange = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+    }
+
+    return (
+        <form className='book-edit' onSubmit={handleSubmit} >
+            <label>Title</label>
+            <input className='input' onChange={handleChange} value={title} />
+            <button className='button is-primary'>
+                Save
+            </button>
         </form>
     )
 }
