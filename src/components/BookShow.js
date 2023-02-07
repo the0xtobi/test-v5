ee88ba30da354a30c4f76837bcb5fe749fae074e
blur <thetobiterra@gmail.com>
2023-02-07 23:42:23 +0100

    comment

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -14,7 +14,7 @@ function BookShow({ book, onDelete, onEdit }) {
 
     // this will serve as the condition that determines what we see/ displayed
     let content = <h3>{book.title}</h3>  // initial value
-    if (showEdit) {
+    if (showEdit) { //if showEdit is true
         content = <BookEdit onEdit={onEdit} book={book} />
     }
 
