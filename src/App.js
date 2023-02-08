cffafa4d7c531444089b4b6057bc17f6e3b701dc
blur <thetobiterra@gmail.com>
2023-02-08 23:22:51 +0100

    comment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   // This state serves as our list
   const [books, setBooks] = useState([])
 
-  // This is the func
+  // This is the func responsible 
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
