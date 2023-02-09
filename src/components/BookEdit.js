565b775c9549d688a09daa2e848ea1eade861609
blur <thetobiterra@gmail.com>
2023-02-09 20:42:46 +0100

    comment

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 function BookEdit({ book, onEdit }) {
+    // We need
     const [title, setTitle] = useState(book.title)
 
     const handleChange = (event) => {
