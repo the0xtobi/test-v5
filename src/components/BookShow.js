2923643b9001517fc4c3e52ded4a78cf5a08e2cf
blur <thetobiterra@gmail.com>
2023-02-12 15:40:56 +0100

    build

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -25,7 +25,7 @@ function BookShow({ book, onDelete, onEdit }) {
                 <button className='edit' onClick={handleEditClick}>
                     Edit
                 </button>
-                <button className="delete" onClick={onDelete(book.id)}>
+                <button className="delete" onClick={()=> onDelete(book.id)}>
                     Delete
                 </button>
             </div>
