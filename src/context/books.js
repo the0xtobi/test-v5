a8d218bf58cecf492d5947a7bc7ff80309154b65
blur <thetobiterra@gmail.com>
2023-02-20 20:26:17 +0100

    build

--- src/context/books.js
+++ src/context/books.js
@@ -12,44 +12,7 @@ const fetchBooks = async () => {
 
   setBooks(response.data)
 }
-const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle
-    })
 
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data }
-      }
-      return book
-    })
-
-    setBooks(updatedBooks)
-  }
-
-  // This function is resposible for book deletion
-  const deleteBookById = async (id) => {
-    const response = await axios.delete(`http://localhost:3001/books/${id}`)
-
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id
-    })
-    setBooks(updatedBooks)
-  }
-
-  // This function is called when the user want to input/create a book
-  const createBook = async (title) => {
-    const response = await axios.post('http://localhost:3001/books',
-      {
-        title: title
-      })
-
-    const updatedBooks = [
-      ...books,
-      response.data
-    ]
-    setBooks(updatedBooks)
-  }
 
     return(
         <BooksContext.Provider value={value} >
