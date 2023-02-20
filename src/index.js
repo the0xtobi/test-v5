9b92aca300d63c01b6bfdfd9473bc76a8bb0ceaa
blur <thetobiterra@gmail.com>
2023-02-20 20:16:45 +0100

    build

--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider>
+        <App />
+    </Provider>);
