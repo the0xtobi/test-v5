1dbb3cf34f4047e9c59f634eefeda0df160b6c32
blur <thetobiterra@gmail.com>
2023-02-20 20:49:46 +0100

    build

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,15 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow'
 
 function BookList({ books, onDelete, onEdit }) {
+   const { books } = useContext(BooksContext)
 // BookList returns BookShow. It's mapped to display one of each book
 // dont forget to pass a key prop!
     return (
         <div className='book-list'>
             {books.map((book) => {
-                return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+                return <BookShow key={book.id} book={book} />
             })}
         </div>
     )
