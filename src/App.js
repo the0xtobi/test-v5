01dde1e6f73f34a3945e99fe9704114bc9c41db4
blur <thetobiterra@gmail.com>
2023-02-20 20:31:18 +0100

    build

--- src/App.js
+++ src/App.js
@@ -10,20 +10,12 @@ function App() {
   useEffect(() => {
     fetchBooks()
   }, [])
-  // This is the func responsible for updating our value. We need to pick an id-
-  // and if the id is true, we update the title with a new title
-  
-
-  
-
-  // This function is resposible for book deletion
-  
 
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList books={books} onEdit={editBookById} onDelete={deleteBookById} />
-      <BookCreate onCreate={createBook} />
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
