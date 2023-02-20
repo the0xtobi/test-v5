25001399db2ac967cbd831f66cfde12be5487602
blur <thetobiterra@gmail.com>
2023-02-20 19:54:41 +0100

    build

--- src/context/books.js
+++ src/context/books.js
@@ -2,3 +2,10 @@ import { createContext } from "react";
 
 const BooksContext = createContext()
 
+function Provider({ children }) {
+    return(
+        <BooksContext.Provider>
+            {children}
+        </BooksContext.Provider>
+    )
+}
