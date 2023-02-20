acc4a6e628c471088a6d92a12ebf238aa6085afa
blur <thetobiterra@gmail.com>
2023-02-20 20:23:25 +0100

    build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import BookCreate from './components/BookCreate'
 import BookList from './components/BookList'
 
@@ -12,44 +12,7 @@ function App() {
   }, [])
   // This is the func responsible for updating our value. We need to pick an id-
   // and if the id is true, we update the title with a new title
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle
-    })
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data }
-      }
-      return book
-    })
-
-    setBooks(updatedBooks)
-  }
-
-  // This function is resposible for book deletion
-  const deleteBookById = async (id) => {
-    const response = await axios.delete(`http://localhost:3001/books/${id}`)
-
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id
-    })
-    setBooks(updatedBooks)
-  }
-
-  // This function is called when the user want to input/create a book
-  const createBook = async (title) => {
-    const response = await axios.post('http://localhost:3001/books',
-      {
-        title: title
-      })
-
-    const updatedBooks = [
-      ...books,
-      response.data
-    ]
-    setBooks(updatedBooks)
-  }
+  
 
   return (
     <div className="app">
