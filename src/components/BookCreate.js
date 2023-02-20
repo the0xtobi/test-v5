2d40ebac49981936bd76cd4eddcfddd2af7e7d33
blur <thetobiterra@gmail.com>
2023-02-20 21:05:55 +0100

    build

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import BooksContext from '../context/books'
 
-function BookCreate({ onCreate }) {
+function BookCreate() {
+    const { BookCreate} =useContext(BooksContext)
     const [title, setTitle] = useState('')
 
     const handleChange = (event) => {
